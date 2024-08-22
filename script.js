5985bf565ffce5de4fc55d1f1aa35b536ea89660
Srhii Shapran <sshaprans@gmail.com>
2024-08-22 17:07:41 +0300

    fitst commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const burger = document.querySelector('.btn-burger');
+
+burger.addEventListener('click', () => {
+    document.querySelector('.nav-bar__wrapper').classList.toggle('is-active');
+})
+
+const swiperBanner = new Swiper(".swiperBanner", {
+    loop:true,
+});
+const swiperProjects = new Swiper(".swiperProjects", {
+    slidesPerView: 1,
+    spaceBetween: 60,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        380: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        780: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 60,
+        },
+    },
+});

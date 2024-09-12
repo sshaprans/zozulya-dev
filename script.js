742fe26e5fec622a492bcac9945ef5071bf643b6
Srhii Shapran <sshaprans@gmail.com>
2024-09-12 03:57:48 +0300

    fixed header

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-
+// header
 const burger = document.querySelector('.btn-burger');
-
 burger.addEventListener('click', () => {
     document.querySelector('.nav-bar__wrapper').classList.toggle('is-active');
 })
+window.onscroll = function() {
+    const header = document.getElementById("header");
+    if (window.scrollY > 0) {
+        header.classList.add("header-opacity");
+    } else {
+        header.classList.remove("header-opacity");
+    }
+};
+
+// form sender
+document.getElementById('contacts').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch('./sendMessage.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            let resultDiv = document.getElementById('form-result');
+            if (data.status === 'success') {
+                resultDiv.innerHTML = `<p style="color: green;">${data.message}</p>`;
+            } else {
+                resultDiv.innerHTML = `<p style="color: red;">${data.message}</p>`;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
+
 // slider
 const swiperBanner = new Swiper(".swiperBanner", {
 
     autoplay: {
-        delay: 3500,
+        delay: 4500,
         disableOnInteraction: false,
     },
     loop:true,
@@ -45,27 +78,28 @@ const swiperProjects = new Swiper(".swiperProjects", {
         },
     },
 });
-
-// form
-document.getElementById('contacts').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    let formData = new FormData(this);
-
-    fetch('./sendMessage.php', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.json())
-        .then(data => {
-            let resultDiv = document.getElementById('form-result');
-            if (data.status === 'success') {
-                resultDiv.innerHTML = `<p style="color: green;">${data.message}</p>`;
-            } else {
-                resultDiv.innerHTML = `<p style="color: red;">${data.message}</p>`;
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+const swiperProjectPage = new Swiper(".swiperProjectPage--thumb", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    watchSlidesProgress: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    loop:true,
+});
+const swiperProjectPageThumb = new Swiper(".swiperProjectPage", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiperProjectPage,
+    },
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    loop:true,
 });
